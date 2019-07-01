ad986451bb7cb4bd84c35a0734b17e00a59ea8f2
adogzZ <15269199005@163.com>
2019-07-01 10:13:37 +0800

    add bootstrap

--- src/main/java/spring/adog/controller/HelloController.java
+++ src/main/java/spring/adog/controller/HelloController.java
@@ -10,6 +10,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(@RequestParam(name = "name") String name, Model model){
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
+    }
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
