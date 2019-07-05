4454580f02513c3cc1349cdbab6642bf1cbc219b
adogzZ <15269199005@163.com>
2019-07-05 16:14:12 +0800

    remove lombok add fix the bug

--- src/main/java/spring/adog/model/User.java
+++ src/main/java/spring/adog/model/User.java
@@ -1,8 +1,6 @@
 package spring.adog.model;
 
-import lombok.Data;
 
-@Data
 public class User {
     private Integer id;
     private String account_id;
@@ -11,4 +9,69 @@ public class User {
     private long gmt_create;
     private long gmt_modified;
     private String avatar_url;
+    private String bio;
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(String account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public long getGmt_create() {
+        return gmt_create;
+    }
+
+    public void setGmt_create(long gmt_create) {
+        this.gmt_create = gmt_create;
+    }
+
+    public long getGmt_modified() {
+        return gmt_modified;
+    }
+
+    public void setGmt_modified(long gmt_modified) {
+        this.gmt_modified = gmt_modified;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
 }
